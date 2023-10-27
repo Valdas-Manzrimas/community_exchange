6cd93fb7df70ff7fe58c64ded3318039edde541d
Valdas Manzrimas <vrajaviharidas@gmail.com>
2023-10-27 18:02:24 +0300

    adding icons and breakpoints

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      xsm: '300px',
+      // => @media (min-width: 300px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       boxShadow: {
         inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.1)',
