c2c4ede87d46b780cd87fd21d2a203a0fe406978
Valdas Manzrimas <vrajaviharidas@gmail.com>
2023-10-30 06:36:09 +0200

    Dropdown btn and small fixes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,13 +7,30 @@ export default {
       boxShadow: {
         inner: 'inset 0 2px 4px 0 rgb(0 0 0 / 0.1)',
       },
+      borderLeft: {
+        primary: '2px solid #166534',
+      },
     },
     colors: {
       transparent: 'transparent',
-      light: '#f0fdf4',
+      light: '#dcfce6',
+
       primary: {
         DEFAULT: '#166534',
         dark: '#052e16',
+      },
+      narvik: {
+        50: '#f0fdf4',
+        100: '#dcfce6',
+        200: '#bbf7cd',
+        300: '#86efa6',
+        400: '#4ade78',
+        500: '#22c554',
+        600: '#16a341',
+        700: '#158036',
+        800: '#16652e',
+        900: '#145327',
+        950: '#052e12',
       },
       dark: '#09090b',
       tahiti: '#3ab7bf',
